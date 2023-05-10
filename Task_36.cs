3460f1a296c3cff4d859df0b2bffcd8959e1f699
fiji <kremus13@gmail.com>
2023-05-10 22:41:12 +0300

    Task_36

---
+++ Task_36.cs
@@ -0,0 +1,31 @@
+int[] newarray = createRandomArray(5);
+
+Console.WriteLine(countNotEvenElem(newarray));
+
+
+int[] createRandomArray(int num)
+{
+    Random rand = new Random();
+    int[] array = new int[num];
+
+    for (int i = 0; i < num; i++)
+    {
+        array[i] = rand.Next(0, 100);
+    }
+    return array;
+}
+
+
+
+int countNotEvenElem(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i += 2)
+    {
+        count += array[i];
+    }
+
+    return count;
+}
+
+
