b1b0d8448b33d5cc12446c3f3214e774ac4f232e
fiji <kremus13@gmail.com>
2023-05-10 22:42:43 +0300

    Task_38

---
+++ Task_38.cs
@@ -0,0 +1,30 @@
+int[] newarray = createRandomArray(8);
+
+Console.WriteLine(Math.Abs((getMinEl(newarray) - getMaxEl(newarray))));
+
+
+int[] createRandomArray(int num)
+{
+    Random rand = new Random();
+    int[] array = new int[num];
+
+    for (int i = 0; i < num; i++)
+    {
+        array[i] = rand.Next(0, 1000);
+    }
+    return array;
+}
+
+int getMinEl(int[] array)
+{
+    return array[0];
+}
+
+int getMaxEl(int[] array)
+{
+    return array[^1];
+}
+
+
+
+
